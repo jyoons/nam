41e4bb23666863cb7de4c3815a5edb78e97fa11c
jiyoon <aroo2005@reflexion.co.kr>
2021-12-27 16:44:13 +0900

    main popup / 게시글 없음

--- project/dist/assets/js/main.js
+++ project/dist/assets/js/main.js
@@ -7,12 +7,18 @@ var mainUi = function (mainUi, $window) {
 
   mainUi.slide = {
     init: function init() {
+      $('body').addClass('scrollOff');
       $('body').append('<div class="dimmed"></div>');
       $('.popup-slide').slick({
+        //         autoplay: true,
+        //   autoplaySpeed: 1000,
+        speed: 200,
         arrows: false,
-        initialSlide: 1 // infinite: false,
-        // slidesToShow: 3,
-        // slidesToScroll: 1
+        initialSlide: 0,
+        dots: true,
+        adaptiveHeight: true,
+        infinite: false // slidesToShow: 3,
+        //  slidesToScroll: 1
 
       });
     }
