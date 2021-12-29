97b9cd01ca3b72d8f0756f77da747dc6eb67e74b
박서연 <seoyeonpub@gmail.com>
2021-12-29 17:29:27 +0900

    main pc 메인배너 수정

--- project/src/assets/js/main.js
+++ project/src/assets/js/main.js
@@ -2,7 +2,9 @@
 
 const mainUi = (function (mainUi, $window) {
     mainUi.init = function () {
-        mainUi.mainPop.init();
+        // mainUi.mainPop.init();
+        $('.main-popup').length > 0 && mainUi.mainPop.init();
+        mainUi.mainVisual.init();
     };
 
     mainUi.mainPop = {
@@ -36,6 +38,48 @@ const mainUi = (function (mainUi, $window) {
             $('body').removeClass('scrollOff');
             $(elem).removeClass('is-active');
             $('.dimmed').remove();
+        }
+    }
+    mainUi.mainVisual = {
+        init: function(){
+            this.mainSlider();
+        },
+        mainSlider : function(){
+            const barAni = 3000;
+
+            $('.main-slider').on('init',function(event,slick){
+                $(this).siblings('.slider-util').find('.bar').css({'animation-duration': (barAni/1000)+0.3+'s'});
+            });        
+            $('.main-slider').slick({
+                dots: true,
+                prevArrow: $('.util-prev'),
+                nextArrow: $('.util-next'),
+                pauseOnHover:false,
+                infinite: true,
+                speed: 1000,
+                fade: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: barAni,
+                customPaging: function(slider,i){
+                    function sliderPage (){
+                        $('.page-num__current').text(i+1);
+                        $('.page-num__total').text(slider.slideCount);
+                    }
+                    return sliderPage();
+                }
+            });
+            $('.main-slider').on('afterChange',function(){
+                $('.progress-bar .bar').addClass('bar-ani');
+            });
+            $('.main-slider').on('beforeChange',function(){
+                $('.progress-bar .bar').removeClass('bar-ani');
+            });
+            // $('.util-pause').on('click',function(){
+            //     $('.main-slider').slick('slickPause');
+            //     $(this).removeClass('on').siblings('.util-play').addClass('on');
+            // });
         }
     }
     mainUi.init();
