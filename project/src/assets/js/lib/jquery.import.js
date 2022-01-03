d6c0392e8158a723b4d85defc3f8fdc8d13156e6
박서연 <seoyeonpub@gmail.com>
2022-01-03 18:26:27 +0900

    main pc css,스크립트 수정

--- project/src/assets/js/lib/jquery.import.js
+++ project/src/assets/js/lib/jquery.import.js
@@ -5,6 +5,7 @@
     codeview.push({ 'url': '/assets/js/lib/gsap.min.js', 'cashbuster': false });
     codeview.push({ 'url': '/assets/js/lib/ScrollTrigger.min.js', 'cashbuster': false });
     codeview.push({ 'url': '/assets/js/lib/slick.min.js', 'cashbuster': false });
+    codeview.push({ 'url': '/assets/js/lib/countUp.umd.js', 'cashbuster': false });
     codeview.push({ 'url': '/assets/js/ui.common.js', 'cashbuster': true });
     for (let a = 0, atotal = codeview.length; a < atotal; a++) {
         document.write('<script src="' + codeview[a].url + ((codeview[a].cashbuster) ? '?cb=' + new Date().getTime() : '') + '" charset="utf-8"></' + 'script>');
